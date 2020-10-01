3d22b8c906da725b66c49113d11ad0e98175da7a
Richard Eisenberg <rae@richarde.dev>
2020-10-01 11:34:14 -0400

    Build uniformity

--- stlc3/lemmas.v
+++
@@ -1,9 +0,0 @@
-Require Import stlcott.
-
-Lemma canonical_forms : 
-  (* WRITE THEOREM HERE *)
-Proof.
-  intros v t1 t2 Hval Htype. destruct v; simpl in Hval; inversion Hval.
-    exists x. exists t. exists v. reflexivity.
-Qed.
-
